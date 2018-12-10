aacdb5d9c6dfbe16f74089794cf1da3228e21849
Corey Emery <cemery@andrew.cmu.edu>
2018-12-10 13:38:29 -0500

    dummy push?

---
+++ build/precache-manifest.1c45ea72f5e3285c2c469271993eb66e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7dcba4965dc696a2806e043ef2362dd",
+    "url": "/pui-demo/static/media/turtle.c7dcba49.jpg"
+  },
+  {
+    "revision": "e207ba84a112d80e0b78c5a841503115",
+    "url": "/pui-demo/static/media/seal.e207ba84.jpg"
+  },
+  {
+    "revision": "3543cbf4bdf41cc5e67ca60fbeea0a50",
+    "url": "/pui-demo/static/media/pig.3543cbf4.jpg"
+  },
+  {
+    "revision": "87e4009a1fb3ec37b8cce209947f493a",
+    "url": "/pui-demo/static/media/fancy.87e4009a.png"
+  },
+  {
+    "revision": "c65be42a767d4b23c155",
+    "url": "/pui-demo/static/js/runtime~main.c65be42a.js"
+  },
+  {
+    "revision": "1ad8c5527749efb9c83d",
+    "url": "/pui-demo/static/js/main.1ad8c552.chunk.js"
+  },
+  {
+    "revision": "e6252a41455e59e77044",
+    "url": "/pui-demo/static/js/1.e6252a41.chunk.js"
+  },
+  {
+    "revision": "1ad8c5527749efb9c83d",
+    "url": "/pui-demo/static/css/main.583db247.chunk.css"
+  },
+  {
+    "revision": "4efa9fa2c68fb020c5d2ba622bea9e0c",
+    "url": "/pui-demo/index.html"
+  }
+];
